2f50550af023919b1325acd70bdd55ce0a93f425
Juan Morais <timrodz@icloud.com>
2023-06-06 23:09:57 +0200

    update supabase lib file

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,26 +1,6 @@
-import { SupabaseClient, User, createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 import { Database } from "./database.types";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseKey);
-
-// export async function getProfile(
-//   supabase: SupabaseClient,
-//   user: User
-// ): Promise<{ user: User,  } | undefined> {
-//   if (!user) {
-//     return undefined;
-//     throw new Error("No user");
-//   }
-
-//   let { data, error, status } = await supabase
-//     .from("profiles")
-//     .select(`username, twitter_profile, avatar_url`)
-//     .eq("id", user.id)
-//     .single();
-
-//   if (error) {
-//     return
-//   }
-// }
